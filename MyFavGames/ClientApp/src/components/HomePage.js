adfc4a2c5dc3d5c7cf65d350f99d846b7c4c9fbc
showboat051 <rashad.davis87@yahoo.com>
2020-10-21 14:57:17 -0500

    initial commit

---
+++ MyFavGames/ClientApp/src/components/HomePage.js
@@ -0,0 +1,13 @@
+﻿import React, { Component } from 'react';
+
+export class HomePage extends Component {
+    static displayName = HomePage.name;
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
