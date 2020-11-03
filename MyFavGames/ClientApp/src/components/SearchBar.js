2c8ded1027180aeffe4ebcfb2cf359aee406108c
showboat051 <rashad.davis87@yahoo.com>
2020-11-03 13:24:08 -0600

    mapped through results

--- MyFavGames/ClientApp/src/components/SearchBar.js
+++ MyFavGames/ClientApp/src/components/SearchBar.js
@@ -5,23 +5,28 @@ export class SearchBar extends Component {
     static displayName = SearchBar.name;
 
     state = {
-        loading: true
+        loading: true,
+        game: null
     }
 
     async componentDidMount() {
-        //
+        
         const url = "https://api.rawg.io/api/platforms?key=abbc22ff4a134bc88501a8419179328a";
         const response = await fetch(url);
         const data = await response.json();
         console.log(data);
-/*        console.log(data.results.id[77].name);
-*/
+        console.log(data.results);
+        let rendRes = data.results.map(sys => {
+            return `${sys.name}`;
+        });
+        console.log(rendRes)
     }
+       // this.setState({game: data.results[0], loading: false})
 
     render() {
         return (
             <div>
-              
+                <rendRes />
             </div>
         )// end of return
     }// end of render 
