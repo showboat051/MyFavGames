6360c99cb3ae1f29f08357a32578474e7196d4ed
showboat051 <rashad.davis87@yahoo.com>
2021-07-01 01:24:50 -0500

    added SNES links

--- MyFavGames/ClientApp/src/components/SnesPage.js
+++ MyFavGames/ClientApp/src/components/SnesPage.js
@@ -12,18 +12,22 @@ export class SnesPage extends Component {
                 <h2> Here are the main games for this console </h2>
                 <ol>
                     <img src={require('../images/MarioWorld.png')} alt="MarioWorld pic" />
-                    <li>Super Mario World </li>
+                    <li><a href = "https://en.wikipedia.org/wiki/Super_Mario_World"> Super Mario World </a></li>
+
                     <img src={require('../images/ZeldaSnes.jpg')} alt="ZeldaSnes pic" />
-                    <li>The Legend of Zelda: A Link to the Past </li>
+                    <li><a href="https://en.wikipedia.org/wiki/The_Legend_of_Zelda:_A_Link_to_the_Past">The Legend of Zelda: A Link to the Past </a> </li>
+
                     <img src={require('../images/superMetroid.jpg')} alt="SuperMetroid pic" />
-                    <li> Super Metroid</li>
+                    <li><a href="https://en.wikipedia.org/wiki/Super_Metroid"> Super Metroid </a></li>
+
                     <img src={require('../images/DKC.jpg')} alt="DKC pic" />
-                    <li> Donkey Kong Country </li>
+                    <li><a href="https://en.wikipedia.org/wiki/Donkey_Kong_Country"> Donkey Kong Country </a></li>
 
                     <img src={require('../images/Superpunchoutbox.jpg')} alt="SPO pic" />
-                    <li> Super PunchOut</li>
+                    <li><a href="https://en.wikipedia.org/wiki/Super_Punch-Out!!">  Super PunchOut </a></li>
+
                     <img src={require('../images/marioKart.jpg')} alt="MarioKart pic" />
-                    <li> Super Mario Kart</li>
+                    <li><a href="https://en.wikipedia.org/wiki/Super_Mario_Kart"> Super Mario Kart </a> </li>
                 </ol>
 
             </div>
